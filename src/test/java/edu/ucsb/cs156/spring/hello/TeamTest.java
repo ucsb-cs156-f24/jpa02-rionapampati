40b7f38e95a7ad1a82007aae82e3216821b5630c
Riona Pampati <riona@ucsb.edu>
2024-10-09 16:40:24 -0700

    all tests pass

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,12 @@ public class TeamTest {
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("f24-14");    
     }
 
     @Test
     public void getName_returns_correct_name() {
-       assert(team.getName().equals("test-team"));
+       assert(team.getName().equals("f24-14"));
     }
 
    
@@ -25,7 +26,30 @@ public class TeamTest {
 
     @Test
     public void toString_returns_correct_string() {
-        assertEquals("Team(name=test-team, members=[])", team.toString());
+        assertEquals("Team(name=f24-14, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_returns_same_object() {
+        Team otherTeam = new Team("f24-14");
+        assertTrue(otherTeam.equals(otherTeam));
+
+        assertTrue(!otherTeam.equals(1));
+
+        Team otherTeam2 = new Team("f24-15");
+        assertTrue(!otherTeam.equals(otherTeam2));
+
+        otherTeam2.addMember("Riona");
+        otherTeam.addMember("Vala");
+
+        assertTrue(!otherTeam.equals(otherTeam2));
+    }
+
+    @Test
+    public void hashCode_returns_same_hash() {
+        Team otherTeam = new Team("f24-14");
+        assertEquals(otherTeam.hashCode(),-1327023895);
+
     }
 
 }
