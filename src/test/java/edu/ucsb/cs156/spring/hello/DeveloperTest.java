8101cf036babbf8677c40f85bddc91168d275d55
Riona Pampati <riona@ucsb.edu>
2024-10-09 16:08:56 -0700

    updated toString

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,9 +25,6 @@ public class DeveloperTest {
         assertEquals("Riona", Developer.getName());
     }
 
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
-
     @Test
     public void getGithubId_returns_correct_githubID() {
         assertEquals("rionapampati", Developer.getGithubId());
